9812bbb55f338a451159c18a05d19e43b0d04614
smcommits <sm.anas@outlook.com>
2021-03-31 00:10:31 +0530

    Add error handling for fetchWeather
    Added error handling for fetchWeather in case the erro returned is a
    type of HttpError with a status code of 404. It checks if the error is
    returned and catches it in a try block

--- src/index.js
+++ src/index.js
@@ -4,11 +4,4 @@ require("babel-polyfill");
 
 import weatherModule from './modules/weather'
 
-/* weatherModule.fetchWeather('New York').then((json) => { */
-  // const weatherData = weatherModule.parseWeatherJSON(json);
-  // weatherModule.getWeatherImage(weatherData.weather).then((imageUrl) => {
-    // console.log(weatherData.weather)
-    // console.log(imageUrl);
-    // document.body.style.backgroundImage = `url${imageUrl}`;
-  // })
-/* }) */
+
--- src/modules/weather.js
+++ src/modules/weather.js
@@ -2,18 +2,35 @@ const weatherModule = (() => {
 
   const locationInput = document.getElementById('location-input')
   const submitButton = document.getElementById('submit-weather-form')
+  const nameElemenet = document.getElementById("name");
+  const tempElement = document.getElementById("temp");
+  const weatherElement = document.getElementById("weather");
+  const humidityElement = document.getElementById("humidity");
+  const dataContainer = document.getElementById("data-container");
+  const loader = document.getElementById("loader-container");
+  const tempToggle = document.getElementById("temp-toggle");
 
 
-  async function fetchWeather(location, unit="metric") {
-    const unitValue = unit == "celcius" ? "metric" : "imperial" 
+  const fetchWeather = async(location, unit=false) =>  {
+   
+    const unitValue = unit == true ? "metric" : "imperial" 
     try {
       const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${unitValue}&appid=4bd4b9ab3b3c5a1d664f8bc0eecfaf36`, {mode: 'cors'});
+      loader.style.display = 'flex';
       const weatherData = await response.json();
-      return weatherData
-    } catch(error) {
-        return "Coudn't find data for the specified location"
+      if (weatherData.cod == "404") {
+        alert("City Not Found")
+        loader.style.display = 'none';
+      } else {
+        return weatherData
+      }
+      
+    } catch(err) {
+      throw err;
     }
   }
+
+  
 
   const parseWeatherJSON = (json) => {
     const temp = json.main.temp
@@ -29,23 +46,55 @@ const weatherModule = (() => {
       headers: {
         "Authorization": "563492ad6f91700001000001abc669dc2c4e46d28693596b6f0a47bf"
       }
+
     }
     )
 
-  const image = await response.json();
+  const image = await response.json()
+    loader.style.display = 'none';
     return image.photos[0].src.original
+  }
+
+  const changeBackground = (url) => {
+    document.getElementById("weather-background").classList.remove("default-weather");
+    document.getElementById("weather-background").classList.add("background-weather");
+    document.getElementById("weather-background").style.backgroundImage = `url(${url})`;
+  }
+
+  const updateInformation = (name, temp, weather, humidity) => {
+    nameElemenet.textContent = name;
+    tempElement.textContent = temp;
+    weatherElement.textContent = weather;
+    humidityElement.textContent = humidity;
+    dataContainer.style.display = "block";
+
   }
 
   submitButton.addEventListener('click', () => {
     const cityName = locationInput.value
-    fetchWeather(cityName).then((json) => {
+    fetchWeather(cityName, tempToggle.checked).then((json) => {
       const weatherData = (parseWeatherJSON(json));
+      updateInformation(weatherData.name, weatherData.temp, weatherData.weather, weatherData.humidity);
       getWeatherImage(weatherData.weather).then((imageUrl) => {
-       document.getElementById("weather-background").classList.remove("default-weather");
-       document.getElementBy("weather-background").classList.add("background-weather");
-        document.getElementById("weather-background").style.backgroundImage = `url(${imageUrl})`;
+        changeBackground(imageUrl);
+      }).catch((err) => {
+        throw "Something went wrong";
       })
-    })
+    }).catch((err) => {
+      throw "Something went wrong";
+    });
+  })
+
+  tempToggle.addEventListener('click', ()=> {
+    let temperature = tempElement.textContent;
+    if(tempToggle.checked) {
+      temperature = Math.round((temperature - 32) * 5/9)
+      tempElement.textContent = temperature;
+    } else {
+      temperature = Math.round((temperature * 9/5 ) + 32)
+      tempElement.textContent = temperature;
+    }
+
   })
   
 
